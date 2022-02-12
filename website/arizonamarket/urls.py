308813a0d7dfec35448ca35ac4c835b71a9dc951
Dmytrenko-Roman <savitarreus@gmail.com>
2022-02-12 12:01:06 +0200

    ❄️ Login/Register ❄️

--- website/arizonamarket/urls.py
+++ website/arizonamarket/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.market, name='market'),
+     path("register", views.register_request, name="register"),
+     path("login", views.login_request, name="login"),
 ]
--- website/website/settings.py
+++ website/website/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'arizonamarket',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
