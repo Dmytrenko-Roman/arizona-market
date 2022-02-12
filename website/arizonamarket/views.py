418fab756ab8142064f78f61da41dc38a9769c47
Dmytrenko-Roman <savitarreus@gmail.com>
2022-02-12 16:44:21 +0200

    ❄️ Black ❄️

--- website/arizonamarket/views.py
+++ website/arizonamarket/views.py
@@ -25,7 +25,7 @@ def register_request(request):
             msg = "Registration successful."
             return redirect("market")
         msg = "Unsuccessful registration. Invalid information."
-		
+
     form = CustomUserCreationForm()
 
     context = {
@@ -39,31 +39,31 @@ def register_request(request):
 
 
 def login_request(request):
-	msg = ''
+    msg = ''
 
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				msg = f"You are now logged in as {username}."
-				return redirect("market")
-			else:
-				msg = "Invalid username or password."
-		else:
-			msg = "Invalid username or password."
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                msg = f"You are now logged in as {username}."
+                return redirect("market")
+            else:
+                msg = "Invalid username or password."
+        else:
+            msg = "Invalid username or password."
 
-	form = AuthenticationForm()
+    form = AuthenticationForm()
 
-	context = {
-		'login_form': form,
-		'message': msg,
-	}
+    context = {
+        'login_form': form,
+        'message': msg,
+    }
 
-	return render(request=request, template_name="market/login.html", context=context)
+    return render(request=request, template_name="market/login.html", context=context)
 
 
 def logout_request(request):
