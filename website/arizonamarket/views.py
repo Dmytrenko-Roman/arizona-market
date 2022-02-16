7b2614848f30c0238f702dffbce7c571b53f0417
Dmytrenko-Roman <savitarreus@gmail.com>
2022-02-16 10:05:34 +0200

    ❄️ Deleted ❄️

--- website/arizonamarket/views.py
+++ website/arizonamarket/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 
 from .models import Car, CustomUser
