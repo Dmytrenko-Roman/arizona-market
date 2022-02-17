783a8c6165c2e56517dc82e9cdf6f4c26988f74f
Dmytrenko-Roman <savitarreus@gmail.com>
2022-02-17 14:10:03 +0200

    ❄️ Removed unused imports ❄️

--- website/arizonamarket/views.py
+++ website/arizonamarket/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic import ListView, CreateView, RedirectView
 from django.contrib.auth.views import LoginView
