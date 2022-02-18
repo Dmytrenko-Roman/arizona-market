192d89ee9c7b23d43b723a25047760ffab81c3c5
Dmytrenko-Roman <savitarreus@gmail.com>
2022-02-18 11:02:21 +0200

    ❄️ Fixed namings ❄️

--- website/arizonamarket/urls.py
+++ website/arizonamarket/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MarketPageView.as_view(), name="market"),
-    path("sell", views.CarSellView.as_view(), name="sell"),
-    path("search", views.SearchView.as_view(), name="search"),
-    path("register", views.RegisterView.as_view(), name="register"),
+    path("", views.MarketPage.as_view(), name="market"),
+    path("sell", views.SellPage.as_view(), name="sell"),
+    path("search", views.SearchPage.as_view(), name="search"),
+    path("register", views.Register.as_view(), name="register"),
     path("login", views.Login.as_view(), name="login"),
-    path("logout", views.LogoutView.as_view(), name="logout"),
+    path("logout", views.Logout.as_view(), name="logout"),
 ]
--- website/arizonamarket/views.py
+++ website/arizonamarket/views.py
@@ -7,13 +7,13 @@ from django.urls import reverse_lazy
 from .models import Car, CustomUser
 from .forms import CustomUserCreationForm, CarSellForm
 
-class MarketPageView(ListView):
+class MarketPage(ListView):
     model = Car
     template_name = "market/market.html"
     context_object_name = "cars"
 
 
-class CarSellView(CreateView):
+class SellPage(CreateView):
     template_name = 'market/sell.html'
     form_class = CarSellForm
     success_url = '/sell'
@@ -22,10 +22,10 @@ class CarSellView(CreateView):
         car = form.save(commit=False)
         car.owner = CustomUser.objects.get(pk=self.request.user.id)
         car.save()
-        return super(CarSellView, self).form_valid(form)
+        return super(SellPage, self).form_valid(form)
 
 
-class SearchView(ListView):
+class SearchPage(ListView):
     model = Car
     template_name = 'market/market.html'
     context_object_name = "cars"
@@ -36,7 +36,7 @@ class SearchView(ListView):
         return cars
 
 
-class RegisterView(CreateView):
+class Register(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'market/register.html'
@@ -52,11 +52,11 @@ class Login(LoginView):
         return super(LoginView, self).form_valid(form)
 
 
-class LogoutView(RedirectView):
+class Logout(RedirectView):
     permanent = False
     query_string = True
     pattern_name = 'market'
 
     def get_redirect_url(self, *args, **kwargs):
         logout(self.request)
-        return super(LogoutView, self).get_redirect_url(*args, **kwargs)
+        return super(Logout, self).get_redirect_url(*args, **kwargs)
